c3a1207b334d61335fc825fb33e3944fee23bacd
quang-Ivan <13552019807>
2022-05-27 17:20:27 +0800

    homework

--- finalprograme.py
+++ finalprograme.py
@@ -7,6 +7,7 @@ import turtle
 
 import pandas as pd
 import numpy as np
+import plotly_express as px
 
 def distances(x, y):
     result = np.dot(np.array(x)-np.array(y),np.array(x)-np.array(y))
@@ -70,6 +71,10 @@ def task2():
         data_frame.to_excel(file_wirte,sheet_name="Shortest_Distance",index=None)
 
 def task3():
+    '''
+    注意先运行taks2()
+    :return:
+    '''
     ori = pd.read_excel("Assignment_Data.xlsx", sheet_name="Shortest_Distance")  # header指选哪行作为剩下数据的表头
     com1 = ["以下为最短的10对城市",None,None]
     com2 = ["以下为最长的10对城市", None, None]
@@ -77,7 +82,7 @@ def task3():
     lst.sort(key = lambda x: x[2])
     for i in lst:
         for j in lst:
-            if i[2]==j[2] and i!=j:
+            if i[1]==j[0] and i[0]==j[1]:
                 lst.remove(j)
     df = []
     df.append(com1)
@@ -91,7 +96,7 @@ def task3():
 
 def task4():
     p = turtle.Turtle()
-    turtle.screensize(1000,800)
+    turtle.setworldcoordinates(-100, -100, 800, 400)
     p.pensize(1)
     p.color('red')
     p.st()
@@ -132,8 +137,72 @@ def task4():
                 p.goto(ci[1])
                 p.dot(5, "blue")
     turtle.done()
+
+def task5():
+    xls = pd.read_excel(r"D:\OneDrive - aierlmalee\whu\python\Assignment_Data.xlsx", sheet_name="CHN144",
+                        header=None)  # header指选哪行作为剩下数据的表头
+    city_names = []
+    X_coordinates = []
+    Y_coordinates = []
+    cities = xls.values
+    for city in cities:
+        X_coordinates.append(city[1])  # 获得满的X坐标
+        Y_coordinates.append(city[2])
+        city_names.append(city[0])
+    coor = [[j * 0.1 for j in i] for i in list(zip(X_coordinates, Y_coordinates))]
+    city_info = list(zip(city_names, coor))
+    fig = px.scatter(X_coordinates, Y_coordinates)
+    ori = pd.read_excel(r"D:\OneDrive - aierlmalee\whu\python\Assignment_Data.xlsx", sheet_name="Shortest_Distance")  # header指选哪行作为剩下数据的表头
+    lst = ori.to_numpy().tolist()
+    short = lst[-21:-11]
+    long = lst[-10:]
+    short_city = []
+    long_city = []
+    for i in short:
+        short_city.append(i[0])
+        short_city.append(i[1])
+    for i in long:
+        long_city.append(i[0])
+        long_city.append(i[1])
+    xss = []
+    yss = []
+    for index,value in enumerate(city_names):
+        for i in short_city:
+            if i==value:
+                xss.append(X_coordinates[index])
+                yss.append(Y_coordinates[index])
+    xlls = []
+    ylls = []
+    for index,value in enumerate(city_names):
+        for i in long_city:
+            if i==value:
+                xlls.append(X_coordinates[index])
+                ylls.append(Y_coordinates[index])
+    X_coordinates=list(set(X_coordinates)-set(xss)-set(xlls))
+    Y_coordinates = list(set(Y_coordinates) - set(yss) - set(ylls))
+    mat = []
+    for i in range(len(X_coordinates)):    #添加normal行
+        ad = ['normal']
+        ad.append(X_coordinates[i])
+        ad.append(Y_coordinates[i])
+        mat.append(ad)
+    for i in range(len(xss)):
+        ad = ['short']
+        ad.append(xss[i])
+        ad.append(yss[i])
+        mat.append(ad)
+    for i in range(len(xlls)):
+        ad = ['long']
+        ad.append(xlls[i])
+        ad.append(ylls[i])
+        mat.append(ad)
+    data_frame = pd.DataFrame(mat,columns=['category',"x",'y'])
+    fig = px.scatter(data_frame,x="x",y="y",color="category")
+    fig.show()
+
 if __name__ == '__main__':
     # task1()
     # task2()
     # task3()
-    task4()
+    # task4()
+    task5()
